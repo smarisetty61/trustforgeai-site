1ca55a1c8278f31f3a462943cda27bdc1ca2e923
smarisetty61 <smarisetty@yahoo.com>
2025-12-22 13:45:10 -0800

    Add files via upload

---
+++ nav.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelector("nav").innerHTML = `
+        <a href="index.html">Home</a>
+        <a href="about.html">About</a>
+        <a href="products.html">Products</a>
+        <a href="technology.html">Technology</a>
+        <a href="ValueProp.html">Value Proposition</a>
+        <a href="team.html">Team</a>
+        <a href="contact.html">Contact</a>
+        <a href="careers.html">Careers</a>
+        <a href="news.html">News</a>
+        <a href="faq.html">Faq</a>
+        <a href="NDA.html">NDA</a>
+    `;
+});
