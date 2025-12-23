1783a2dd2f633d677b5c74efa26805366326b770
smarisetty61 <smarisetty@yahoo.com>
2025-12-23 11:08:39 -0800

    Update nav.js

--- nav.js
+++ nav.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
     document.querySelector("nav").innerHTML = `
         <a href="index.html">Home</a>
-        <a href="about.html">About</a>
         <a href="technology.html">Technology</a>
         <a href="ValueProp.html">Value Proposition</a>
         <a href="products.html">Products</a>
         <a href="team.html">Team</a>
+        <a href="about.html">About</a>
         <a href="contact.html">Contact</a>
         <a href="careers.html">Careers</a>
         <a href="news.html">News</a>
@@ -13,4 +13,5 @@ document.addEventListener("DOMContentLoaded", () => {
         <a href="NDA.html">NDA</a>
     `;
 });
+
 
